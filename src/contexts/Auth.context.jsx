00aba78d0aebeb45e9be4a733cddb9850585ee1c
Laura Pinto <laurampinto90@gmail.com>
2024-08-15 18:02:41 +0100

    error fixed on context

--- src/contexts/Auth.context.jsx
+++ src/contexts/Auth.context.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { API_URL } from "../config";
 
 const AuthContext = createContext();
 
@@ -16,7 +17,7 @@ const AuthWrapper = ({ children }) => {
   const authenticateUser = async () => {
     try {
       const tokenFromStorage = localStorage.getItem("authToken");
-      const { data } = await axios.get("http://localhost:5005/auth/verify", {
+      const { data } = await axios.get(`${API_URL}/auth/verify`, {
         headers: { authorization: `Bearer ${tokenFromStorage}` },
       });
       console.log("verify route successful", data);
